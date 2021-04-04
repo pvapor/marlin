4ec92ec0e251be6efa9fd6b7637da36fa8ef5a82
CheerAmi <2291381998@qq.com>
2021-04-04 10:35:53 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ sudo apt update && sudo apt -y dist-upgrade
 sudo apt install -y gcc g++ python make texinfo texlive bc bison build-essential ccache curl flex g++-multilib gcc-multilib \
     git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev \
     libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev \
-    unzip language-pack-zh-hans
+    unzip language-pack-zh-hans build-essential libncurses5-dev
 # GCC Cross
 wget "https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/heads/android10-qpr1-b-release.tar.gz"
 gzip -d  android10-qpr1-b-release.tar.gz
